ba55ca4f73a1c9fd0cc5a3621db3fb5beb391ccb
darryl west <darryl.west@RainCitySoftware.com>
2015-10-16 12:06:21 -0700

    made changes to menu

--- sandbox/electron-atom/toptal-loki/src/common/menu.js
+++ sandbox/electron-atom/toptal-loki/src/common/menu.js
@@ -6,40 +6,52 @@
 var remote = require('remote'),
     Menu = remote.require('menu');
 
-module.exports = {
-    create: function() {
-        var appMenu = Menu.buildFromTemplate([
+var template = [
+    {
+        label: 'My App',
+        submenu: [
             {
-                label: 'Electron',
-                submenu: [{
-                    label: 'Credits',
-                    click: function() {
-                        alert('Built with Electron & Loki.js.');
-                    }
-                }]
+                label: 'Credits',
+                click: function() {
+                    alert('Built with Electron & Loki.js.');
+                }
             },
             {
-                label: 'File',
-                submenu: [
-                    {
-                        label: 'Create Password',
-                        accelerator: 'CmdOrCtrl+N',
-                        click: function() {
-                            // alert('Create new password');
-                            ipc.send('toggle-insert-view');
-                        }
-                    },
-                    {
-                        type: 'separator'
-                    },
-                    {
-                        label: 'Quit',
-                        accelerator: 'CmdOrCtrl+Q',
-                        selector: 'terminate:' //osx only
-                    }
-                ]
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'CmdOrCtrl+Q',
+                selector: 'terminate:' //osx only
+            } 
+        ]
+    },
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Create Password',
+                accelerator: 'CmdOrCtrl+N',
+                click: function() {
+                    // alert('Create new password');
+                    ipc.send('toggle-insert-view');
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'CmdOrCtrl+Q',
+                selector: 'terminate:' //osx only
             }
-        ]);
+        ]
+    }
+];
+
+module.exports = {
+    create: function() {
+        var appMenu = Menu.buildFromTemplate( template );
 
         Menu.setApplicationMenu(appMenu);
     }
